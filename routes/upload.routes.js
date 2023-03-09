99cbcfdace4c27ba088657de3085c1866a8622f9
Özgür Eker <oeker55@gmail.com>
2023-03-09 16:56:40 +0300

    upload image

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -7,7 +7,7 @@ const { API_URL } = require("../constants");
 // path.dirname;
 const router = express.Router();
 
-const upload = multer({ dest: "./public" });
+const upload = multer({ dest: "./public/images" });
 
 router.post("/", upload.single("upload"), async (req, res) => {
   const photo = req.file;
@@ -24,13 +24,13 @@ router.post("/", upload.single("upload"), async (req, res) => {
       extName === "image/tif" ||
       extName === "image/bmp"
     ) {
-      const targetPathUrl = path.join(photo.destination, "logo.PNG");
+      const targetPathUrl = path.join(photo.destination, photo.originalname);
 
       fs.renameSync(PathPhoto, targetPathUrl);
       res.json({
         uploaded: true,
 
-        url: `${API_URL}/logo.PNG`,
+        url: `${API_URL}/${photo.originalname}`,
       });
     } else {
       fs.unlinkSync(photo.path);
@@ -43,3 +43,49 @@ router.post("/", upload.single("upload"), async (req, res) => {
 });
 
 module.exports = router;
+
+// const express = require("express");
+// const multer = require("multer");
+// const fs = require("fs");
+// const path = require("path");
+// const { API_URL } = require("../constants");
+
+// // path.dirname;
+// const router = express.Router();
+
+// const upload = multer({ dest: "./public" });
+
+// router.post("/", upload.single("upload"), async (req, res) => {
+//   const photo = req.file;
+
+//   var PathPhoto = photo.path;
+
+//   const extName = photo.mimetype;
+//   try {
+//     if (
+//       extName === "image/png" ||
+//       extName === "image/jpeg" ||
+//       extName === "image/jfif" ||
+//       extName === "image/jpg" ||
+//       extName === "image/tif" ||
+//       extName === "image/bmp"
+//     ) {
+//       const targetPathUrl = path.join(photo.destination, "logo.PNG");
+
+//       fs.renameSync(PathPhoto, targetPathUrl);
+//       res.json({
+//         uploaded: true,
+
+//         url: `${API_URL}/logo.PNG`,
+//       });
+//     } else {
+//       fs.unlinkSync(photo.path);
+
+//       res.json({ message: "Geçersiz Dosya Uzantısı!" });
+//     }
+//   } catch (error) {
+//     res.json(error);
+//   }
+// });
+
+// module.exports = router;
